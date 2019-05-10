c4a5c38692b1d75fda2873a1c79ff390b492ffab
ykohki <43834721+ykohki@users.noreply.github.com>
2019-05-10 23:59:48 +0900

    test

--- skggenometracks/tracks/MethylationTrack.py
+++ skggenometracks/tracks/MethylationTrack.py
@@ -1,69 +1,102 @@
 # -*- coding: utf-8 -*-
+
+from . BedGraphTrack import BedGraphTrack
 from . GenomeTrack import GenomeTrack
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 class MethylationTrack(GenomeTrack):
-    SUPPORTED_ENDINGS = ['.mr']  #mr=methylation rate
+    # this track class extends a BedGraphTrack that is already part of
+    # pyGenomeTracks. The advantage of extending this class is that
+    # we can re-use the code for reading a bedgraph file
+    SUPPORTED_ENDINGS = ['mr']
     TRACK_TYPE = 'methylation_rate_graph'
-    OPTIONS_TXT = """
-height = 3
-title =
-text =
-# x position of text in the plot (in bp)
-x position =
-    """.format(TRACK_TYPE)
+    OPTIONS_TXT = GenomeTrack.OPTIONS_TXT + """
+        # bismark file which describe 1bp resolution methylation rate.
+        # E.g.
+        # chr1    10563   10563   90.9090909090909        10      1
+        # chr1    10571   10571   83.3333333333333        10      2
+        file_type = {}
+        """.format(TRACK_TYPE)
 
-    def __init__(self, *args, **kwargs):
-        super(self.__class__, self).__init__(*args, **kwargs)
+    def __init__(self, properties_dict):
+        self.properties = properties_dict
 
-        import pandas as pd
-        import numpy as np
+        if 'alpha' not in self.properties:
+            self.properties['alpha'] = 1
+        if 'color' not in self.properties:
+            self.properties['color'] = '#FF0080'
+        if 'size' not in self.properties:
+            self.properties['size'] = 10
 
-        import matplotlib as mpl
-        import matplotlib.pyplot as plt
-        import pylab
-
-        #from skggenometracks.plotTracks import get_region(region_string)
-
-        #self.bw = pyBigWig.open(self.properties['file'])
-        input_df=pd.read_table(self.properties['file'],header=None)
-        #test_dfにcolumnsを追加
-        input_df.columns = ["chr", "start", "end","methyl_sum","de_methyl_sum","per_methyl","low","ratio","high"]
 
     def plot(self, ax, chrom_region, start_region, end_region):
-        formated_region = "{}:{}-{}".format(chrom_region, start_region, end_region)
+        """
+        Args:
+           ax: matplotlib axis to plot
+           chrom_region: chromosome name
+           start_region: start coordinate of genomic position
+           end_region: end coordinate
+        """
 
-        import pandas as pd
-        import numpy as np
+        # the BedGraphTrack already has methods to read files
+        # in which the first three columns are chrom, start,end
+        # here we used the get_scores method inherited from the
+        # BedGraphTrack class
 
-        import matplotlib as mpl
-        mpl.use('TkAgg')
-        import matplotlib.pyplot as plt
-        import pylab
+        # print(chrom_region, start_region, end_region)
 
-        from skggenometracks.plotTracks import main
-        #from skggenometracks.plotTracks import parse_arguments
-        #args = parse_arguments().parse_args(args)
-
-        input_df=pd.read_table(self.properties['file'],header=None)
-        input_df.columns = ["chr", "start", "end","methyl_sum","de_methyl_sum","per_methyl","low","ratio","high"]
+        df = pd.read_csv(self.properties['file'], header=None, sep='\t')
+        df.columns =  ["chr", "start", "end","methyl_sum","de_methyl_sum","per_methyl","low","ratio","high"]
         #startとendの中央をとる
-        input_df["end"]=input_df["end"]-1000
+        df["center"] = df["end"]-1000
 
-        chr_input_df=input_df[input_df['chr'] == chrom_region]
-        range_chr_df=chr_input_df[(chr_input_df['end'] >= start_region) & (chr_input_df['end'] <= end_region)]
+        df_q = df[df['chr'] == 'chr'+str(chrom_region)]
+        df_s= df_q.query('@start_region <= center & center <= @end_region ')
+        #fig = plt.figure()
+        #ax = fig.add_subplot(1,1,1)
+        ax.plot(df_s['ratio'])
+        #ax.plot(df_s['ratio'],alpha=self.properties['alpha'],
+        #        color=self.properties['color'])
+        ax.set_ylim(-5,105)
 
+    def plot_y_axis(self, ax, plot_axis):
+        """
+        Plot the scale of the y axis with respect to the plot_axis
+        Args:
+            ax: axis to use to plot the scale
+            plot_axis: the reference axis to get the max and min.
+        Returns:
+        """
+        if 'show data range' in self.properties and self.properties['show data range'] == 'no':
+            return
 
-        pylab.figure(figsize=(10, 4), dpi=200)
+        def value_to_str(value):
+            # given a numeric value, returns a
+            # string that removes unneeded decimal places
+            if value % 1 == 0:
+                str_value = str(int(value))
+            else:
+                str_value = "{:.1f}".format(value)
+            return str_value
 
-        # X軸の数字をオフセットを使わずに表現する
-        plt.gca().get_xaxis().get_major_formatter().set_useOffset(False)
+        ymin, ymax = 0, 1
 
-        #グラフに凡例をつける
-        plt.legend(["ratio"])
-        plt.ylim([0, 1.0])
+        ymax_str = value_to_str(ymax)
+        ymin_str = value_to_str(ymin)
+        # plot something that looks like this:
+        # ymax ┐
+        #      │
+        #      │
+        # ymin ┘
 
-        plt.plot(range_chr_df["ratio"])
-        plt.fill_between(range_chr_df.index,range_chr_df["low"], range_chr_df["high"], color="blue", alpha=0.2)
-
-        plt.show()
-        #plt.savefig(outFileName.png)
+        # the coordinate system used is the ax.transAxes (lower left corner (0,0), upper right corner (1,1)
+        # this way is easier to adjust the positions such that the lines are plotted complete
+        # and not only half of the width of the line.
+        x_pos = [0, 0.5, 0.5, 0]
+        y_pos = [0.01, 0.01, 0.99, 0.99]
+        ax.plot(x_pos, y_pos, color='black', linewidth=1, transform=ax.transAxes)
+        ax.text(-0.2, -0.01, ymin_str, verticalalignment='bottom', horizontalalignment='right', transform=ax.transAxes)
+        ax.text(-0.2, 1, ymax_str, verticalalignment='top', horizontalalignment='right', transform=ax.transAxes)
+        ax.patch.set_visible(False)
