ed89d014b33da90c9bc620bed1983bad6e88ccd3
yyoshiaki <y.yasumizu@gmail.com>
2019-10-17 23:40:21 +0900

    Update MethylationTrack.py

--- skggenometracks/tracks/MethylationTrack.py
+++ skggenometracks/tracks/MethylationTrack.py
@@ -61,10 +61,16 @@ class MethylationTrack(GenomeTrack):
         # startとendの中央をとる
         # half_window_step makes the 'center' of 'start' and 'end'
         # df["center"] = df["end"]-int(self.properties['half_window_step'])
+
         df["center"] = df.apply(
             lambda x: int(
                 (x['start'] + x['end']) / 2),
-            axis=1, meta=(None, 'int64'))
+            axis=1)
+
+        # df["center"] = df.apply(
+        #     lambda x: int(
+        #         (x['start'] + x['end']) / 2),
+        #     axis=1, meta=(None, 'int64'))
 
         # E.g. chrom_region=chrX
         df_q = df[df['chr'] == str(chrom_region)]
